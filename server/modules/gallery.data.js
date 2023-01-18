248af3e1c109ddb6f1420b5b47983a020c0f30f0
Krys Frost <librarykrystal@gmail.com>
2023-01-18 01:17:54 -0600

    Images added, galleryItems array updated.

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,10 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/catacombs.jpg', description: 'Not a single one had anything to comb.', likes: 13 },
+    { id: 2, path: 'images/moulinRouge.jpg', description: 'It uses electricity.', likes: 99 },
+    { id: 3, path: 'images/thinker.jpg', description: 'Oh, I know this one! Just give me a minute.', likes: 29 },
+    { id: 4, path: 'images/notreDame.jpg', description: 'Sunburned to a crisp.', likes: 0 },
+    { id: 5, path: 'images/pomponOwl.jpg', description: 'A striking sil-chouette.', likes: 72 },
+    { id: 6, path: 'images/nike.jpg', description: 'Some nice stairs.', likes: 17 }
 ];
 
 module.exports = galleryItems;
