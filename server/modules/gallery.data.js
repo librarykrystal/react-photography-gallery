52866409726f1932a6b0c979f830203fa7f79be2
Krys Frost <librarykrystal@gmail.com>
2023-01-18 04:23:32 -0600

    Two images added.

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -2,9 +2,11 @@ const galleryItems = [
     { id: 1, path: 'images/catacombs.jpg', description: 'Not a single one had anything to comb.', likes: 13 },
     { id: 2, path: 'images/moulinRouge.jpg', description: 'It uses electricity.', likes: 99 },
     { id: 3, path: 'images/thinker.jpg', description: 'Oh, I know this one! Just give me a minute.', likes: 29 },
-    { id: 4, path: 'images/notreDame.jpg', description: 'Sunburned to a crisp.', likes: 0 },
-    { id: 5, path: 'images/pomponOwl.jpg', description: 'A striking sil-chouette.', likes: 72 },
-    { id: 6, path: 'images/nike.jpg', description: 'Some nice stairs.', likes: 17 }
+    { id: 4, path: 'images/mannequin.jpg', description: 'Who nose?', likes: 54 },
+    { id: 5, path: 'images/notreDame.jpg', description: 'Sunburned to a crisp.', likes: 0 },
+    { id: 6, path: 'images/pomponOwl.jpg', description: 'A striking sil-chouette.', likes: 72 },
+    { id: 7, path: 'images/nike.jpg', description: 'Some nice stairs.', likes: 17 },
+    { id: 8, path: 'images/pantheonCrypt.jpg', description: 'This guy nose.', likes: 1 }
 ];
 
 module.exports = galleryItems;
