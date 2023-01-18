fd2eb429e1ff26887a1087959636f174a3a55cb8
Krys Frost <librarykrystal@gmail.com>
2023-01-18 03:10:11 -0600

    Axios GET request written, working.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
+// import component(s)
 
 function App() {
+    const [galleryArray, setGalleryArray] = useState([]);
+
+    console.log('galleryArray TEST:', galleryArray);
+
+    // calling GET function on page load
+    useEffect( () => {
+      fetchGallery();
+    }, [])
+
+    // retrieving gallery contents with axios GET
+    // router is pre-written to grab this data from gallery.data.js module
+    const fetchGallery = () => {
+      axios.get('/gallery')
+      .then((response) => {
+        console.log('GET Response data:', response.data);
+        setGalleryArray(response.data);
+      }).catch(function(error) {
+        console.log('GET Error', error);
+      });
+    }
+
+
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Gallery of My Life</h1>
+          <h1 className="App-title">Gallery of My Snaps</h1>
         </header>
         <p>Gallery goes here</p>
         <img src="images/pomponOwl.jpg"/>
