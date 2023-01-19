8ed274462e289425991ade12934a18c5199cae64
Krys Frost <librarykrystal@gmail.com>
2023-01-19 00:08:54 -0600

    Created and styled placeholder like buttons and like counts. Each photo has a styled border.

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,10 +1,11 @@
 function GalleryItem(props){
     return(
-        <>
+        <div className="photo">
         {/* <p>{JSON.stringify(props)}</p>    TEST */}
         <img key={props.id} src={props.photo}></img>
-
-        </>
+        <button className="likeBtn">LIKE</button>
+        <p className="likesP">♥︎ 0</p>
+        </div>
     )
 }
 
