b11f4bf19df5596df491a1b0b993386df6b77d27
Krys Frost <librarykrystal@gmail.com>
2023-01-20 21:06:59 -0600

    STRETCH goal: PUT route to database completed.

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const galleryItems = require('../modules/gallery.data');
-
-// Additions for database STRETCH GOAL:
+// Addition for database STRETCH GOAL:
 const pool = require('../modules/pool.js');
 
 // DATABASE GET
 router.get("/", (req, res) => {
-    console.log("In router!");
+    console.log("GET");
+    // queryText gets all items from DB and orders them by ascending ID (so future photos would add to end)
     let queryText = 'SELECT * from "gallery" ORDER BY "id" ASC; ';
     pool
       .query(queryText)
       .then((result) => {
-        console.log("results from db", result.rows);
+        console.log("results from db:", result.rows);
         res.send(result.rows);
       })
       .catch((error) => {
@@ -22,7 +22,21 @@ router.get("/", (req, res) => {
   });
 
 // DATABASE PUT
-// TODO!
+router.put("/like/:id", (req, res) => {
+    console.log("PUT req.params.id:", req.params.id);
+    // queryText tells DB to update by adding 1 to the likes count of the item with this ID:
+    let queryText = `UPDATE "gallery" SET "likes" = likes + 1 WHERE id =${req.params.id}; `;
+    pool
+      .query(queryText)
+      .then((result) => {
+        console.log('PUT result:', result);
+        res.sendStatus(202);
+      })
+      .catch((error) => {
+        console.log("PUT error", error);
+        res.sendStatus(500);
+      });
+  });
 
 
 
